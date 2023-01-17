6eea0096e63c17a953a44df2545776ec3ce19e8a
AsareelDon <asareeldonpena@gmail.com>
2023-01-17 19:47:38 +0800

    initial rebase login ui

--- frontend/lib/screens/auth/login_page.dart
+++ frontend/lib/screens/auth/login_page.dart
@@ -15,130 +15,212 @@ class LoginPage extends StatefulWidget {
 class _SignInState extends State<LoginPage> {
   final Authentication _authService = Authentication();
   String email = '', password = '';
+  late bool _password;
+  @override
+  void initState() {
+    super.initState();
+    _password = false;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(125, 125, 125, 1.0),
+      backgroundColor: const Color.fromRGBO(5, 26, 47, 1.0),
       body: Form(
-        child: Center(
-          child: Container(
-            height: 400,
-            width: 470,
-            margin: const EdgeInsets.all(20),
-            child: Card(
-              color: const Color.fromRGBO(61, 59, 59, 1.0),
-              elevation: 10,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10))
-              ),
-              child: ListView(
-                padding: const EdgeInsets.all(20),
-                children: [
-                  const Text('Sign in to Clone', style: TextStyle(color: Colors.blue, fontSize: 27, fontWeight: FontWeight.bold),),
-                  const SizedBox(height: 20,),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      labelStyle: const TextStyle(color: CupertinoColors.white),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(
-                          color: CupertinoColors.activeBlue,
-                        ),
-                      ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 90, right: 20, left: 20),
+          child: Center(
+            child: ListView(
+              children: [
+                const Text('Git sync to everyone in the branch.', style: TextStyle(color: Colors.white, fontSize: 27, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 35,),
+                TextFormField(
+                  style: const TextStyle(color: CupertinoColors.systemGrey2),
+                  decoration: InputDecoration(
+                    labelText: 'Email ro username',
+                    labelStyle: const TextStyle(color: CupertinoColors.systemGrey2),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: CupertinoColors.activeBlue,)
                     ),
-                    onChanged: (val) => setState(() {
-                      email = val;
-                    }),
                   ),
-                  const SizedBox(height: 20,),
-                  TextFormField(
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      labelStyle: const TextStyle(color: CupertinoColors.white),
-                      border: OutlineInputBorder(
+                  onChanged: (val) => setState(() {
+                    email = val;
+                  }),
+                ),
+                const SizedBox(height: 20,),
+                TextFormField(
+                  obscureText: !_password,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  style: const TextStyle(color: CupertinoColors.systemGrey2),
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: const TextStyle(color: CupertinoColors.systemGrey2),
+                    focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(
-                          color: CupertinoColors.activeBlue,
-                        ),
-                      ),
+                        borderSide: const BorderSide(color: CupertinoColors.activeBlue,)
                     ),
-                    onChanged: (val) => setState(() {
-                      password = val;
-                    }),
+                    suffixIcon: IconButton(
+                      icon: Icon(_password ? Icons.visibility : Icons.visibility_off, color: CupertinoColors.systemGrey, size: 20,),
+                      onPressed: () {
+                        setState(() {
+                          _password = !_password;
+                        });
+                      },
+                    ),
                   ),
-                  const SizedBox(height: 30,),
-                  ElevatedButton(
+                  onChanged: (val) => setState(() {
+                    password = val;
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10, left: 200),
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan> [
+                        TextSpan(
+                            text: 'Forgot password?',
+                            style: const TextStyle(color: CupertinoColors.systemBlue, fontSize: 13),
+                            recognizer: TapGestureRecognizer()..onTap = () async{
+                              _authService.login(email, password);
+                            }
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(35),
-                      )
+                      ),
                     ),
                     onPressed: () async => {
-                      _authService.login(email, password)
+                      _authService.login(email, password),
+                      /**if(email.isNotEmpty || password.isNotEmpty){
+                        _authService.login(email, password),
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.grey.shade900,
+                              ),
+                              child: const Text('Required field can''t be empty.'),
+                            ),
+                            behavior: SnackBarBehavior.floating,
+                          )
+                        )
+                      } else {
+                        ///dli nko ma trace ang authentication kai wla ko aha na file
+                        if (authentication of account is invalid){
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Container(
+                                  color: Colors.grey.shade900,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: const Text('Invalid username and password.'),
+                                ),
+                              )
+                          )
+                        }
+                      } **/
                     },
-                    child: const Text("Login")
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                       shape: RoundedRectangleBorder(
-                         borderRadius: BorderRadius.circular(35),
-                      )
+                    child: const Text("Login", style: TextStyle(color: CupertinoColors.white, fontWeight: FontWeight.bold),)
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const[
+                    Expanded(
+                      child: Divider(
+                        endIndent: 10,
+                        indent: 10,
+                        thickness: 1,
+                        color: CupertinoColors.systemGrey2,
+                      ),
                     ),
-                    onPressed: () async => {
-                      _authService.login(email, password)
-                    },
-                    child: const Text("Forgot password?", style: TextStyle(color: CupertinoColors.white),)
+                    SizedBox(width: 8),
+                    Text(
+                      'or',
+                      style: TextStyle(color: CupertinoColors.systemGrey2),
+                    ),
+                    SizedBox(width: 8),
+                    Expanded(
+                      child: Divider(
+                        endIndent: 10,
+                        indent: 10,
+                        thickness: 1,
+                        color: CupertinoColors.systemGrey2,
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(35),
+                      )
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'Don''t have an account?',
-                        style: const TextStyle(color: Colors.white, fontSize: 13),
-                        children: <TextSpan> [
-                          TextSpan(
+                  onPressed: () async => {
+                    _authService.login(email, password)
+                  },
+                  child: Row(
+                    children: const [
+                      ///credit to Icon8 for the twitter icon
+                      SizedBox(width: 50,),
+                      Image(
+                        image: AssetImage('assets/images/google_icon96.png'),
+                        height: 30,
+                        width: 30,
+                      ),
+                      SizedBox(width: 5,),
+                      Text("Continue with Google", style: TextStyle(color: CupertinoColors.black, fontWeight: FontWeight.bold),)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Don''t have an account?',
+                      style: const TextStyle(color: CupertinoColors.systemGrey2, fontSize: 13),
+                      children: <TextSpan> [
+                        TextSpan(
                             text: ' Signup',
                             style: const TextStyle(color: CupertinoColors.systemBlue, fontSize: 13),
                             recognizer: TapGestureRecognizer()..onTap = () {
-                              /// wla pko kabalo kaau sa routing format so printing sa ta ani nga part
-                              debugPrint('SignUp module is not yet created!');
+                              ///RegisterPage();
                             }
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
       ),
       bottomNavigationBar: SizedBox(
-        height: 80,
-        child: WaveWidget( //user Stack() widget to overlap content and waves
+        height: 20,
+        width: 400,
+        child: WaveWidget(
           config: CustomConfig(
-            colors: [
-              Colors.blue.withOpacity(0.3),
-              Colors.blue.withOpacity(0.3),
-              Colors.blue.withOpacity(0.3),
-              //the more colors here, the more wave will be
+            durations: [35000, 19440, 10800, 6000],
+            heightPercentages: [0.20, 0.23, 0.25, 0.30],
+            blur: const MaskFilter.blur(BlurStyle.outer, 10),
+
+            colors: const [
+              Colors.white70,
+              Colors.white54,
+              Colors.white30,
+              Colors.white24,
             ],
-            durations: [4000, 5000, 7000],
-            //durations of animations for each colors,
-            // make numbers equal to numbers of colors
-            heightPercentages: [0.01, 0.05, 0.03],
-            //height percentage for each colors.
-            blur: const MaskFilter.blur(BlurStyle.solid, 5),
-            //blur intensity for waves
           ),
-          waveAmplitude: 17.00, //depth of curves
-          waveFrequency: 3, //number of curves in waves
-          ///backgroundColor: Colors.white, //background colors
+          waveAmplitude: 7.25,
           size: const Size(
             double.infinity,
             double.infinity,
@@ -148,3 +230,4 @@ class _SignInState extends State<LoginPage> {
     );
   }
 }
+
